bd0006095b5d494947698f2721673991c8de8dcb
paddlehands88 <stefancorlett@hotmail.com>
2024-03-07 17:51:40 +0000

    Follow Service completed 4:58:57. Functionality created to follow and unfollow other users. Toast info messages working and prisma db Follow table and related User table fields tested for updates

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,15 +1,33 @@
+import { notFound } from "next/navigation";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+import { Actions } from "./_components/actions";
+
+
 interface UserPageProps {
   params: {
     username: string;
   };
 };
 
-const UserPage = ({
+const UserPage = async ({
   params
 }: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+
   return (
-    <div>
-      User: {params.username}
+    <div className="flex flex-col gap-y-4">
+      <p>username: {user.username}</p>
+      <p>user ID: {user.id}</p>
+      <p>isFollowing: {`${isFollowing}`}</p>
+      <Actions userId={user.id} isFollowing={isFollowing} />
     </div>
   );
 }
