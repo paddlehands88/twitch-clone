b787af42cbf7bde01d0290d2be7f6e8984245893
paddlehands88 <stefancorlett@hotmail.com>
2024-03-08 13:12:21 +0000

    Follow service functionality within sidebar completed and tested

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,16 +1,21 @@
+import { getFollowedUsers } from "@/lib/follow-service";
+import { getRecommended } from "@/lib/recommended-service";
+
 import { Toggle, ToggleSkeleton } from "./toggle";
+import { Following, FollowingSkeleton } from "./following";
 import { Wrapper } from "./wrapper";
 import { Recommended, RecommendedSkeleton } from "./recommended";
 
-import { getRecommended } from "@/lib/recommended-service";
 
 export const Sidebar = async () => {
   const recommended = await getRecommended();
+  const following = await getFollowedUsers();
 
   return (
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
+        <Following data={following} />
         <Recommended data={recommended} />
       </div>
     </Wrapper>
@@ -21,6 +26,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
