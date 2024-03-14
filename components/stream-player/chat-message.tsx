9daaef5a74f0053f34ad01a2cc2fe4f77e4ef031
paddlehands88 <stefancorlett@hotmail.com>
2024-03-14 09:49:22 +0000

    community module completed. Had to regress version of usehooks-ts to enable useDebounce. Also updated loading skeletons for stream player. App working although testing on community components limited to single logged-in user

--- components/stream-player/chat-message.tsx
+++ components/stream-player/chat-message.tsx
@@ -22,8 +22,8 @@ export const ChatMessage = ({
       <div className="flex flex-wrap items-baseline gap-1 grow">
         <p className="text-sm font-semibold whitespace-nowrap">
           <span className="truncate" style={{ color: color }}>
-            {data.from?.name}
-          </span>:
+            {data.from?.name}:
+          </span>
         </p>
         <p className="text-sm break-all">
           {data.message}
--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -10,6 +10,7 @@ import {
 import { OfflineVideo } from "./offline-video";
 import { LoadingVideo } from "../loading-video";
 import { LiveVideo } from "./live-video";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface VideoProps {
   hostName: string;
@@ -43,3 +44,11 @@ export const Video = ({
     </div>
   )
 }
+
+export const VideoSkeleton = () => {
+  return (
+    <div className="aspect-video border-x border-background">
+      <Skeleton className="h-full w-full rounded-none" />
+    </div>
+  );
+};
