41dc809f5c53c08573479502f097e4a390fa2966
paddlehands88 <stefancorlett@hotmail.com>
2024-03-15 10:01:29 +0000

    Header Component (at bottom of stream player) completed. Includes host viewer (google) (dynamic Offline/live) avatar, stream user's name and dynamic Offline/Online+Viewer number(s) and responsive Unfollow/follow button

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,9 +7,10 @@ import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 
-import { Video, VideoSkeleton } from "./video";
-import { Chat, ChatSkeleton } from "./chat";
+import { Header, HeaderSkeleton } from "./header";
 import { ChatToggle } from "./chat-toggle";
+import { Chat, ChatSkeleton } from "./chat";
+import { Video, VideoSkeleton } from "./video";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -50,9 +51,17 @@ return (
         )}
       >
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
-      <Video
+        <Video
+              hostName={user.username}
+              hostIdentity={user.id}
+            />
+        <Header
             hostName={user.username}
             hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
           />
       </div>
         <div
@@ -81,7 +90,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/* TODO: Header Skeleton Component */}
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
