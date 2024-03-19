1d0db2756ba375f731c76b1010b78d68708864ab
paddlehands88 <stefancorlett@hotmail.com>
2024-03-19 14:03:21 +0000

    Homepage section complete with some further error handling. 5:55:38 At time of posting LiveBade is working in sidebar. Follow Service doesn't work when attempting to follow someone duriung a live stream. Also LiveKit have said a egres limit has been hit, & planet scale functionality will terminate on 9th April

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,8 +1,15 @@
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
+
 export default function Page() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Home Page</h1>
-      
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      Home Page
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
+      <Results />
     </div>
   );
-}
+};
